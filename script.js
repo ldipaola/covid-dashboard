868fcba2fb10e60de2a5cfb4899c3745cb1d5bd1
Lukey <ldipaola66@gmail.com>
2020-06-20 11:54:08 +1000

    add api get request

---
+++ script.js
@@ -0,0 +1,18 @@
+fetch("https://covid-193.p.rapidapi.com/statistics", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "covid-193.p.rapidapi.com",
+		"x-rapidapi-key": "510f15014bmsh6ce0dd84c132d79p116676jsna21790da67cc"
+	}
+})
+.then(response => response.ok ? response.json() : console.log("Error"))
+.then(data =>{
+    console.log(data);
+
+    document.querySelector("body").textContent = JSON.stringify(data);
+})
+.catch(err => {
+	console.log(err);
+});
+
+
